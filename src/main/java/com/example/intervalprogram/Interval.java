919c2f69f1652eab107949814b17fb80879e93d6
Vladimir <vovas11@gmail.com>
2018-03-26 13:52:11 +0200

    Add Interval entity

--- src/main/java/com/example/intervalprogram/Interval.java
+++ src/main/java/com/example/intervalprogram/Interval.java
@@ -1,4 +1,60 @@
 package com.example.intervalprogram;
 
+/**
+ * Immutable data holder representing an entity of interval
+ */
 public class Interval {
+    private final int start;
+    private final int end;
+
+    /**
+     * Constructor makes sure that the field {@code start} gets a lower value
+     *
+     * @param start starting number of the interval
+     * @param end ending number of the interval
+     */
+    public Interval(int start, int end) {
+        if (end < start) {
+            this.start = end;
+            this.end = start;
+        } else {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Interval)) return false;
+
+        Interval interval = (Interval) o;
+
+        return start == interval.start && end == interval.end;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start;
+        result = 31 * result + end;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
+    }
 }
--- src/test/java/com/example/intervalprogram/IntervalTest.java
+++ src/test/java/com/example/intervalprogram/IntervalTest.java
@@ -1,4 +1,37 @@
 package com.example.intervalprogram;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class IntervalTest {
+
+
+    private static final Interval intervalCorrect = new Interval(2, 100);
+    private static final Interval intervalInCorrect = new Interval(100, 2);
+
+    @Test
+    public void testConstructorLogic() {
+        assertEquals(intervalCorrect.getStart(), intervalInCorrect.getStart());
+        assertEquals(intervalCorrect.getEnd(), intervalInCorrect.getEnd());
+    }
+
+    @Test
+    public void testEqualsNotInstances() {
+        assertFalse(intervalCorrect == intervalInCorrect);
+    }
+
+    @Test
+    public void testEqualsMethodEquals() {
+        assertTrue(intervalCorrect.equals(intervalInCorrect));
+    }
+
+    @Test
+    public void testEqualsMethodHashCode() {
+        assertEquals(intervalCorrect.hashCode(), intervalInCorrect.hashCode());
+    }
+
+
 }
